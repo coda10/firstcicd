2c5dd9652a8c731e9fcb0c3ff85fe88675237794
Francis Badasu <nytro04@gmail.com>
2021-10-27 20:32:20 +0000

    .

--- Desktop/django begins/myfirstproject/firstApp/serializers.py
+++ Desktop/django begins/myfirstproject/firstApp/serializers.py
@@ -1,8 +1,8 @@
-from django.db import models
-from . import serializers 
+
+from rest_framework import serializers 
 from .models import Employee
 
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = '__all__'
+        fields = '__all_'
