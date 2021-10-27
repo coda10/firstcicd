dc7c5c282fa861c8f25b6a41b19f24152d235ff6
Francis Badasu <nytro04@gmail.com>
2021-10-27 18:24:57 +0000

    Heroku hosting setup

--- Desktop/django begins/myfirstproject/myfirstproject/settings.py
+++ Desktop/django begins/myfirstproject/myfirstproject/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c#pktwt%pr-j@=3nx##2_#77+wn%^9mf$f_m78a$$uaml(-m7!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myfirstdjangobackend.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
